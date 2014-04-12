eb132006499901f1e649ab3000a526a6b718d24c
heipacker <heipackermail@gmail.com>
2014-04-12 11:15:22 +0800

    add class struct

--- src/cplusplusexample.cpp
+++ src/cplusplusexample.cpp
@@ -8,8 +8,58 @@
 
 #include <iostream>
 using namespace std;
+class Shape{
+
+public:
+	string toString(){
+		return NULL;
+	}
+};
+/**
+ *¾ØÐÎ
+ */
+class Rectangle:Shape{
+private:
+	int width, height;
+public:
+	Rectangle();
+
+	void set_values(int, int);
+
+	int area() {
+		return width * height;
+	}
+
+	int getWidth(){
+		return width;
+	}
+
+	int getHeight(){
+		return height;
+	}
+
+	void setWidth(int width){
+		this->width = width;
+	}
+
+	void setHeight(int height){
+		this->height = height;
+	}
+};
+
+Rectangle::Rectangle() {
+	width = 5;
+	height = 5;
+}
+
+void Rectangle::set_values(int x, int y) {
+	width = x;
+	height = y;
+}
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	Rectangle rect;
+	rect.set_values(3, 4);
+	cout << "area: " << rect.area() << endl;
 	return 0;
 }
